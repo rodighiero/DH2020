839534def5a3e5fd53cc75a2eae8823bf58e3e6b
Dario Rodighiero <2089004+rodighiero@users.noreply.github.com>
2020-08-11 21:47:36 +0200

    Fit at start

--- src/elements/pixi.js
+++ src/elements/pixi.js
@@ -1,6 +1,9 @@
 import * as PIXI from 'pixi.js'
 import { Viewport } from 'pixi-viewport'
 import arialDataPNG from '../constant/arial.png'
+import { scaleLinear } from 'd3-scale'
+import { extent } from "d3-array"
+
 
 export default (arialXML) => {
 
@@ -16,52 +19,55 @@ export default (arialXML) => {
         autoResize: true,
         resizeTo: window,
     })
-    document.body.prepend(app.view)
+    document.body.append(app.view)
 
-    s.app = app
+    // Font
 
     const arialPNG = PIXI.Texture.from(arialDataPNG)
-    const arial = PIXI.BitmapFont.install(arialXML, arialPNG)
+    PIXI.BitmapFont.install(arialXML, arialPNG)
 
     // Create and append viewport
 
-    const viewport = new Viewport({
+    s.pixi = new Viewport({
         screenWidth: window.innerWidth,
         screenHeight: window.innerHeight,
         interaction: app.renderer.plugins.interaction
     })
-    app.stage.addChild(viewport)
+    app.stage.addChild(s.pixi)
 
-    s.pixi = viewport
-    
-    // Activate
+    // Set scales
 
-    const zoomMin = .3
-    const zoomMax = 5
+    const extX = extent(s.nodes, d => d.x)
+    const extY = extent(s.nodes, d => d.y)
+    const width = extX[1] - extX[0]
+    const height = extY[1] - extY[0]
+    const scaleX = window.innerWidth / width
+    const scaleY = window.innerHeight / height
+    const scale = scaleX < scaleY ? scaleX : scaleY
+    const zoomMin = scale
+    const zoomMax = 3
 
-    viewport
+    // Set vieport
+
+    s.pixi
         .drag()
         .pinch()
         .wheel()
         .decelerate()
-        .clampZoom({ minScale: zoomMin, zoomMax: zoomMax })
-        .setTransform(window.innerWidth / 2, window.innerHeight / 2, zoomMin, zoomMin)
+        .clampZoom({ minScale: zoomMin, maxScale: zoomMax })
+        .setTransform(window.innerWidth / 2, window.innerHeight / 2, scale, scale)
 
     // Transparency on zoom
+    // 0. Background 1. Links 2. Contours 3. Keywords 4. Nodes 5. Wordclouds
 
-    const links = 1
-    const contours = d3.scaleLinear().domain([zoomMin, 2]).range([1, 0])
-    const couples = d3.scaleLinear().domain([zoomMin, 2]).range([1, 0])
-    const nodes = 1
-    const triplets = d3.scaleLinear().domain([zoomMin, 2]).range([0, 1])
+    const zoomOut = scaleLinear().domain([zoomMin, 2]).range([1, 0])
+    const zoomIn = scaleLinear().domain([zoomMin, 2]).range([0, 1])
 
-    viewport.on('zoomed', e => {
+    s.pixi.on('zoomed', e => {
         const scale = e.viewport.lastViewport.scaleX
-        e.viewport.children[0].alpha = links
-        e.viewport.children[1].alpha = contours(scale)
-        e.viewport.children[2].alpha = couples(scale)
-        e.viewport.children[3].alpha = nodes
-        e.viewport.children[4].alpha = triplets(scale)
+        e.viewport.children[2].alpha = zoomOut(scale)
+        e.viewport.children[3].alpha = zoomOut(scale)
+        e.viewport.children[5].alpha = zoomIn(scale)
     })
 
     // Prevent pinch gesture in Chrome
